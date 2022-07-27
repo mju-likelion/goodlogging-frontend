ac12cba766ab72635c07bfb2b9bba05adf711807
lyj0047 <lyj0047@naver.com>
2022-07-27 16:15:30 +0900

    markdown: 로그인 페이지

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,21 @@
 const createConfig = require('@titicaca/eslint-config-triple/create-config')
 
-module.exports = createConfig({ type: 'frontend' })
+const { extends: extentsConfigs, overrides } = createConfig({
+  type: 'frontend',
+})
+
+module.exports = {
+  extends: [...extentsConfigs],
+  overrides: [...overrides],
+  rules: {
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'import/no-unresolved': 'off',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+      },
+    },
+  },
+}
