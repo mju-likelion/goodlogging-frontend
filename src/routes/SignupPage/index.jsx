441a335e7f4f9ff9a01daec505e75d1e2df3315e
BHyeonKim <rlaqhguse@gmail.com>
2022-08-02 17:15:47 +0900

    refactor: 버튼 및 네비게이션 컴포넌트 분리

--- src/routes/SignupPage/index.jsx
+++ src/routes/SignupPage/index.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-import Button from '../../components/Button'
+import Button from '../../components/Buttons/Button'
 
 import styles from './signupPage.module.scss'
 
